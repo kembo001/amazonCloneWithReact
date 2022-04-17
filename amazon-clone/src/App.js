e18332ade695361dcf046e2ac2ec634d50256350
Brandon K <brandonk@Brandons-MacBook-Pro.local>
2022-04-17 15:30:42 -0500

    added checkout page

--- amazon-clone/src/App.js
+++ amazon-clone/src/App.js
@@ -2,15 +2,27 @@
 import './App.css';
 import Header from './Header';
 import Home from './Home'
+import {BrowserRouter as Router, Switch, Route}
+from "react-router-dom"
 
 function App() {
   return (
-    <div className="App">
-     {/* Header */}
-     <Header/>
-     {/* Home */}
-     <Home />
-    </div>
+    <Router>
+      <div className="App">
+
+        <Switch>
+          <Route path = '/checkout'>
+            <Header/>
+          </Route>
+
+          <Route path = '/'>
+            <Header/>
+            <Home />
+          </Route>
+        </Switch>
+     
+      </div>
+    </Router>
   );
 }
 
