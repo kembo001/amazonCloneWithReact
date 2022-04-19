887dc331c1fffea122e59642806ec7f5f5c2ae10
Brandon K <brandonk@Brandons-MacBook-Pro.local>
2022-04-19 09:50:57 -0500

    styles checkout page

--- amazon-clone/src/Checkout.js
+++ amazon-clone/src/Checkout.js
@@ -1,19 +1,20 @@
 import React from 'react'
 import './Checkout.css'
+import Subtotal from './Subtotal'
 
 function Checkout() {
   return (
 <div className = 'checkout'>
     <div className= 'checkoutLeft'>
-     {/* <img className = 'checkoutAD' src = 'https://m.media-amazon.com/images/I/61Ppgg4eycL.jpg' 
-     alt=''/> */}
+     <img className = 'checkoutAD' src = 'https://i0.wp.com/digiday.com/wp-content/uploads/2018/01/amazon_money.jpg?fit=682%2C284&quality=100&strip=all&ssl=1' 
+     alt=''/>
      
      <div>
         <h2 className='checkoutTitle'>Your Shopping Basket</h2>
      </div>
       </div>
     <div className = 'checkoutRight'>
-       <h2>The Subtotal</h2>
+       <Subtotal/>
     </div>
 
 </div>
--- amazon-clone/src/Header.js
+++ amazon-clone/src/Header.js
@@ -2,12 +2,14 @@ import React from 'react'
 import './Header.css'
 import SearchIcon from '@material-ui/icons/Search';
 import LocalMallIcon from '@material-ui/icons/LocalMall';
+import {Link} from "react-router-dom";
 
 function Header() {
   return (
     <div className='header'>
-      <img className='header_logo' src='https://upload.wikimedia.org/wikipedia/commons/thumb/a/a9/Amazon_logo.svg/2560px-Amazon_logo.svg.png' alt=''/>
-
+      
+      <Link to= '/'><img className='header_logo' src='https://upload.wikimedia.org/wikipedia/commons/thumb/a/a9/Amazon_logo.svg/2560px-Amazon_logo.svg.png' alt=''/>
+      </Link>
       <div className='header_search'>
           <input className='header_searchInput' type= 'text' />
           <SearchIcon className='header__searchIcon'/>
@@ -29,11 +31,12 @@ function Header() {
           <span className='header_optionLineOne'>Your</span>
           <span className='header_optionLineTwo'>Prime</span>
         </div>
-        
+      <Link to="/checkout">
         <div className='header_optionBasket'>
           <LocalMallIcon className='cart'/>
           <span className='header_optionLineTwo header_basketCount'>0</span>
         </div>
+      </Link>
       </div>
 
     </div>
