b2d023c05841ecfd43ab7bd755ef4f26aabccaba
kembo001 <47574348+kembo001@users.noreply.github.com>
2022-04-22 22:44:03 -0500

    Designed Login Page

--- amazon-clone/src/Login.js
+++ amazon-clone/src/Login.js
@@ -8,6 +8,26 @@ function Login() {
     <Link to= '/'>
         <h1 className='loginLogo'><b>Amazon</b></h1>
     </Link>
+
+    <div className='loginContainer'>
+        <h1>Sign In</h1>
+
+        <form>
+            <h5>E-mail</h5>
+            <input type='text' />
+
+            <h5>Password</h5>
+            <input type='password' />
+
+            <button className='loginSignInButton'>Sign In</button>
+        </form>
+
+        <p>By continuing, you agree to Amazon's Conditions of Use and Privacy Notice.
+        </p>
+
+        <button className='SignupButton'>Don't have an account? Sign up here. </button>
+
+    </div>
 </div>
   )
 }
